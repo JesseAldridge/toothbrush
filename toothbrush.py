b2254b27c071a1e7863c6cb996d604a05e48c083
Jesse Aldridge <JesseAldridge@gmail.com>
2017-11-25 18:00:44 -0800

    cleanup

--- toothbrush.py
+++ toothbrush.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-import sys, tty, termios, subprocess, os, json
+import sys, tty, termios, subprocess, os, json, glob
 
-DIR_PATH = os.path.expanduser("~/Dropbox/toothbrush_notes")
+DIR_PATH = os.path.expanduser("~/Dropbox/tbrush_notes")
 
 def getch():
   # Return a single character from stdin.
@@ -20,12 +20,8 @@ def main_loop():
 
   notes = Notes()
   query_string = ''
-  mode = 'search'
   while True:
-    print '-' * 10
-    print 'query: {}'.format(query_string)
-    print '-' * 10
-    print mode.upper()
+    print '-- query: {} --'.format(query_string)
 
     ch = getch()
     if ord(ch) == 3:  # ctrl+c
@@ -33,7 +29,7 @@ def main_loop():
     elif ord(ch) == 127:  # backspace
       query_string = query_string[:-1]
     elif ord(ch) == 13:  # return
-      if len(notes.matched_filenames) == 0:
+      if len(notes.matched_basenames) == 0:
         notes.new_note(query_string)
       notes.open_all()
       break
@@ -45,61 +41,59 @@ class Notes:
   def __init__(self):
     self.dir_path = os.path.expanduser(DIR_PATH)
     self.meta_path = os.path.expanduser('~/.toothbrush')
-    self.files = {}
-    for filename in os.listdir(self.dir_path):
-      file_path = os.path.join(self.dir_path, filename)
-      if os.path.isdir(file_path):
-        continue
-      with open(file_path) as f:
-        self.files[filename] = f.read()
+    self.basename_to_content = {}
+    glob_path = os.path.join(self.dir_path, '*.txt')
+    for path in glob.glob(glob_path):
+      basename = os.path.splitext(os.path.basename(path))[0]
+      with open(path) as f:
+        self.basename_to_content[basename] = f.read()
 
-    self.filename_to_open_count = {}
+    self.basename_to_open_count = {}
     if os.path.exists(self.meta_path):
       with open(self.meta_path) as f:
         text = f.read()
       try:
-        self.filename_to_open_count = json.loads(text)
+        self.basename_to_open_count = json.loads(text)
       except Exception:
         pass
 
   def search(self, query_string):
-    self.matched_filenames = []
+    self.matched_basenames = []
 
     terms = set(query_string.split())
-    for filename, content in self.files.iteritems():
+    for basename, content in self.basename_to_content.iteritems():
       for term in terms:
-        if term not in filename and term not in content:
+        if term not in basename and term not in content:
           break
       else:
-        self.matched_filenames.append(filename)
+        self.matched_basenames.append(basename)
 
-    self.matched_filenames.sort(key=lambda filename: self.filename_to_open_count.get(filename, 0))
+    self.matched_basenames.sort(key=lambda basename: self.basename_to_open_count.get(basename, 0))
 
-    for i, filename in enumerate(self.matched_filenames[:10]):
-      prefix = '{}) '.format(i)
-      print '{}{}'.format(prefix, filename)
+    for basename in self.matched_basenames[:10]:
+      print basename
 
-    if not self.matched_filenames:
+    if not self.matched_basenames:
       print '~ nothing found ~'
 
   def open_all(self):
-    for filename in self.matched_filenames[:10]:
-      path = os.path.join(self.dir_path, filename)
+    for basename in self.matched_basenames[:10]:
+      path = os.path.join(self.dir_path, basename)
       self.open_path(path)
 
   def open_path(self, path):
     print 'opening:', path
 
-    filename = os.path.basename(path)
-    self.filename_to_open_count.setdefault(filename, 0)
-    self.filename_to_open_count[filename] += 1
+    basename = os.path.basename(path)
+    self.basename_to_open_count.setdefault(basename, 0)
+    self.basename_to_open_count[basename] += 1
     with open(self.meta_path, 'w') as f:
-      f.write(json.dumps(self.filename_to_open_count))
+      f.write(json.dumps(self.basename_to_open_count))
 
     os.system('open "{}"'.format(path))
 
   def new_note(self, query_string):
-    new_path = os.path.join(DIR_PATH, query_string)
+    new_path = os.path.join(DIR_PATH, query_string) + '.txt'
     with open(new_path, 'w') as f:
       f.write('')
     self.open_path(new_path)
